b63f4b5fd02b4ea0be32fe41313fe5c714c3e69d
jiaqi <1140103268@qq.com>
2017-07-10 22:42:40 +0800

    add home

---
+++ July8/src/components/pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="box">
+   <myheader></myheader>
+</div>
+</template>
+
+<script>
+import myheader from '../common/header'
+export default {
+  components: {
+     myheader
+  }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
--- July8/src/router/index.js
+++ July8/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import home from '@/components/pages/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/home',
+      name: 'home',
+      component: home
     }
   ]
 })
