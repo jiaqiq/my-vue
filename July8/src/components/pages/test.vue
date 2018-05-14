952732b349e37eca37fa0d27cdbe0fe312cbd138
jiaqiq <1140103268@qq.com>
2018-05-14 15:56:04 +0800

    增删改查

--- July8/src/components/pages/test.vue
+++ July8/src/components/pages/test.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="test">
         <h3>测试node</h3>
-        <button type="primary" @click="getNodeText">请求数据</button>
+        <button type="primary" @click="getNodeText">增加数据</button>
+        <button type="primary" @click="delData">删除数据</button>
+        <button type="primary" @click="update">修改数据</button>
+        <button type="primary" @click="selectData">查询数据</button>
         <div>{{result}}</div>
     </div>
 </template>
@@ -28,6 +31,39 @@ export default {
           })
           .catch(err => {
             console.log(127, err)
+          })
+        },
+        delData() {
+          this.$http.post("/api/users/delUser?id=2").then(res => {
+            console.log('delete', res)
+            this.result = res.data;
+          })
+        .catch(err => {
+          console.log(err)
+        })
+        },
+        update() {
+          // let data = {
+          //   id:5,
+          //   name:'更新',
+          //   age: 28,
+          //   teacher: '刘老师'
+          // }
+          this.$http.post("/api/users/updateUser?id=21&name=李四&age=24&teacher=刘老师").then(res => {
+            console.log('update', res) 
+            this.result = res.data;
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        },
+        selectData() {
+          this.$http.post("api/users/selectUser?id=88").then(res => {
+            console.log('selectData', res)
+            this.result = res.data;
+          })
+          .catch(err => {
+            console.log(err)
           })
         },
         transfer(num) {
